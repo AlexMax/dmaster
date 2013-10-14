22f4bfba1d47b42737a231a192648d27dea0a386
Alex Mayfield <alexmax2742@gmail.com>
2013-10-14 19:43:21 -0400

    Parse MSC_BEGINSERVERLISTPART messages.
    A lot of building out has been done, in addition to the message parsing
    the huffman encoder and decoder has been stubbed out and works with
    unencoded messages.

--- huffman.js
+++ huffman.js
@@ -1,5 +1,5 @@
 function createHuffman(freq) {
-	if (!(freq instanceof Array)) {
+	if (!Array.isArray(freq)) {
 		throw new TypeError('First argument must be an array.');
 	}
 
@@ -9,14 +9,27 @@ function createHuffman(freq) {
 
 	return {
 		encode: function(data) {
+			if (!Buffer.isBuffer(data)) {
+				throw new TypeError('Argument must be a Buffer.');
+			}
 
+			// Do no encoding yet...
+			encoded = new Buffer(data.length + 1);
+			encoded.writeUInt8(0xff, 0);
+			data.copy(encoded, 1);
+			return encoded;
 		},
 		decode: function(data) {
+			if (data.readUInt8(0) === 0xff) {
+				// Data does not need to be decoded.
+				decoded = new Buffer(data.length - 1);
+				data.copy(decoded, 0, 1);
+				return decoded;
+			}
 
+			throw new Error('Huffman decoding not implemented yet.');
 		}
 	}
 }
 
-exports = {
-	createHuffman: createHuffman
-};
+exports.createHuffman = createHuffman;
