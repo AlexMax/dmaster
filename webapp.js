6e93357d88633f064599890818f36313175a3de7
Alex Mayfield <alexmax2742@gmail.com>
2013-10-16 23:48:57 -0400

    Separate out REST and static page rendering.
    This commit also brings in some bits of HTML5 boilerplate into the static directory.

--- webapp.js
+++ webapp.js
@@ -3,28 +3,38 @@ var db = require('./db.js');
 
 var webapp = express();
 
-webapp.get('/', function(req, res) {
-	res.redirect(301, 'servers');
-});
-webapp.get('/servers', function(req, res) {
-	db.all('SELECT * FROM servers;', function(err, rows) {
-		res.send(rows);
-	});
-});
-webapp.get('/servers/:address::port', function(req, res) {
-	var address = req.params.address;
-	var port = req.params.port;
+// App configuration
+webapp.use(express.logger());
+webapp.use(express.compress());
+webapp.use('/static', express.static('static'));
 
-	db.get(
-		'SELECT * FROM servers AS s ' +
-		'WHERE s.address = ? AND s.port = ?;'
-	, address, port, function(err, row) {
-		if (row) {
-			res.send(row);
-		} else {
-			res.send(404);
-		}
-	});
+// App routes
+webapp.get('/', function(req, res) {
+	res.send('hello, world!');
 });
+
+// REST routes v1
+(function(prefix) {
+	webapp.get(prefix + '/servers', function(req, res) {
+		db.all('SELECT * FROM servers;', function(err, rows) {
+			res.send(rows);
+		});
+	});
+	webapp.get(prefix + '/servers/:address::port', function(req, res) {
+		var address = req.params.address;
+		var port = req.params.port;
+
+		db.get(
+			'SELECT * FROM servers AS s ' +
+			'WHERE s.address = ? AND s.port = ?;'
+		, address, port, function(err, row) {
+			if (row) {
+				res.send(row);
+			} else {
+				res.send(404);
+			}
+		});
+	});
+})('/api/v1');
 
 module.exports = webapp;
