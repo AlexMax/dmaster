0a9a0531d77cb9e26a80a5e48df18fc6c434321f
Alex Mayfield <alexmax2742@gmail.com>
2013-10-19 23:53:24 -0400

    Add map to the visible template.

--- dmaster.js
+++ dmaster.js
@@ -414,8 +414,10 @@ socket.on('message', function(msg, rinfo) {
 		break;
 	case zan.SERVER_LAUNCHER_CHALLENGE:
 		var serverInfo = unmarshallServerInfo(data.slice(4));
-		var stmt = 'UPDATE servers SET name=?, updated=datetime(\'now\') WHERE address = ? AND port = ?;'
-		db.run(stmt, serverInfo.name, rinfo.address, rinfo.port, function(error) {
+		var stmt =
+			'UPDATE servers SET name=?, map=?, updated=datetime(\'now\') ' +
+			'WHERE address = ? AND port = ?;';
+		db.run(stmt, serverInfo.name, serverInfo.map, rinfo.address, rinfo.port, function(error) {
 			if (error) {
 				throw new Error(error);
 			}
